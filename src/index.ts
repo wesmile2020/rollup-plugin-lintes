78a6572f689ade7620060a16b002615f703718cc
微笑 <1211921908@qq.com>
2020-11-17 00:00:17 +0800

    modify lint es

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import { createFilter, FilterPattern } from '@rollup/pluginutils';
+import { ESLint } from 'eslint';
+import fs from 'fs';
+import rollup from 'rollup';
+import { printLintResult } from './log';
+
+interface Options {
+    include?: FilterPattern;
+    exclude?: FilterPattern;
+}
+
+const defaultOptions: Options = {
+    exclude: 'node_modules',
+};
+
+function lint(options: Options = {}): rollup.Plugin {
+    const opts = { ...defaultOptions, ...options };
+    const filter = createFilter(opts.include, opts.exclude);
+    const cli = new ESLint({});
+
+    const exitSet: Set<string> = new Set();
+
+    return {
+        name: 'rollup-plugin-lintes',
+
+        async load(id) {
+            const lintIgnore = await cli.isPathIgnored(id);
+            if (lintIgnore || !filter(id)) return null;
+            if (fs.existsSync(id)) {
+                exitSet.add(id);
+            } else {
+                exitSet.delete(id);
+                return null;
+            }
+            try {
+                const result = await cli.lintFiles(id);
+                const isHasError = printLintResult(result, id);
+                if (isHasError) {
+                    throw new Error('rollup-plugin-lintes: load error');
+                }
+            } catch (error) {
+                throw error;
+            }
+            return null;
+        },
+
+        // 针对 vue 中的 script 进行校验
+        async transform (code: string, id: string) {
+            const lintIgnore = await cli.isPathIgnored(id);
+            if (lintIgnore || !filter(id) || exitSet.has(id)) return null;
+            
+            try {
+                const result = await cli.lintText(code, { filePath: id });
+                const isHasError = printLintResult(result, id);
+                if (isHasError) {
+                    throw new Error('rollup-plugin-lintes: transform error');
+                }
+            } catch (error) {
+                throw error;
+            }
+            return null;
+        },
+    };
+}
+
+export { lint };
+export default lint;
+
---
+++ src/log.ts
@@ -0,0 +1,29 @@
+import chalk from 'chalk';
+import { ESLint, Linter } from 'eslint';
+
+export function logError(...rest: any) {
+    console.log(chalk.bold.red(...rest));
+}
+
+export function printLintMessages(messages: Linter.LintMessage[], id: string) {
+    for (let i = 0; i < messages.length; i += 1) {
+        const  msgArr = [
+            `${id} (${messages[i].line + 1},${messages[i].column + 1}): error`,
+            `${messages[i].ruleId}: ${messages[i].message}`,
+        ];
+        logError(msgArr.join('\n'));
+    }
+}
+
+export function printLintResult(result: ESLint.LintResult[] ,id: string) {
+    let isHasErr = false;
+    for (let i = 0; i < result.length; i += 1) {
+        if (result[i].errorCount > 0) {
+            isHasErr = true;
+        }
+        if (result[i].errorCount + result[i].warningCount > 0) {
+            printLintMessages(result[i].messages, id);
+        }
+    }
+    return isHasErr;
+}
