4170c21ca5c8dc997045ddf5c3d793b2db5e0393
weixiao <1211921908@qq.com>
2020-11-19 18:10:58 +0800

    publish new version

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { printLintResult } from './log';
 interface Options {
     include?: FilterPattern;
     exclude?: FilterPattern;
+    failOnError?: boolean;
 }
 
 const defaultOptions: Options = {
@@ -18,46 +19,23 @@ function lint(options: Options = {}): rollup.Plugin {
     const filter = createFilter(opts.include, opts.exclude);
     const cli = new ESLint({});
 
-    const exitSet: Set<string> = new Set();
 
     return {
         name: 'rollup-plugin-lintes',
 
         async load(id) {
             const lintIgnore = await cli.isPathIgnored(id);
-            if (lintIgnore || !filter(id)) return null;
-            if (fs.existsSync(id)) {
-                exitSet.add(id);
-            } else {
-                exitSet.delete(id);
-                return null;
-            }
+            if (lintIgnore || !filter(id) || !fs.existsSync(id)) return null;
             try {
                 const result = await cli.lintFiles(id);
                 const isHasError = printLintResult(result, id);
-                if (isHasError) {
+                if (isHasError && opts.failOnError) {
                     throw new Error('rollup-plugin-lintes: load error');
                 }
             } catch (error) {
-                throw error;
-            }
-            return null;
-        },
-
-        // 针对 vue 中的 script 进行校验
-        async transform (code: string, id: string) {
-            const lintIgnore = await cli.isPathIgnored(id);
-            if (lintIgnore || !filter(id) || exitSet.has(id)) return null;
-            
-            try {
-                const filePath = id.split('?')[0];
-                const result = await cli.lintText(code, { filePath });
-                const isHasError = printLintResult(result, filePath);
-                if (isHasError) {
-                    throw new Error('rollup-plugin-lintes: transform error');
+                if (opts.failOnError) {
+                    throw error;
                 }
-            } catch (error) {
-                throw error;
             }
             return null;
         },
