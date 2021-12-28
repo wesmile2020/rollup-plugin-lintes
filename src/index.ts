c2a77e4b661b77bf17919cb01354436c2d7008d6
lei3.li <lei3.li@ecarxgroup.com>
2021-12-28 14:55:10 +0800

    feat: update eslint useage

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { createFilter, FilterPattern } from '@rollup/pluginutils';
 import { ESLint } from 'eslint';
 import fs from 'fs';
 import rollup from 'rollup';
-import { printLintResult } from './log';
 
 interface Options {
     include?: FilterPattern;
@@ -18,7 +17,7 @@ function lint(options: Options = {}): rollup.Plugin {
     const opts = { ...defaultOptions, ...options };
     const filter = createFilter(opts.include, opts.exclude);
     const cli = new ESLint({});
-
+    let formatter: ESLint.Formatter | undefined;
 
     return {
         name: 'rollup-plugin-lintes',
@@ -28,7 +27,18 @@ function lint(options: Options = {}): rollup.Plugin {
             if (lintIgnore || !filter(id) || !fs.existsSync(id)) return null;
             try {
                 const result = await cli.lintFiles(id);
-                const isHasError = printLintResult(result, id);
+                let isHasError = false;
+                for (let i = 0; i < result.length; i += 1) {
+                    if (result[i].errorCount > 0) {
+                        isHasError = true;
+                        break;
+                    }
+                }
+                if (!formatter) {
+                    formatter = await cli.loadFormatter('stylish');
+                }
+                const resultText = formatter.format(result);
+                console.log(resultText);
                 if (isHasError && opts.failOnError) {
                     throw new Error('rollup-plugin-lintes: load error');
                 }
